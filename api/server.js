37d542c90e6b7ac167a2b19eb3244ec19375e352
Ssenkula Rivan <ssenkula.rivan@gmail.com>
2026-07-06 18:33:15 -0700

    Simplify Vercel config

--- api/server.js
+++
@@ -1,6 +0,0 @@
-// Vercel Serverless Function Entry Point
-export default async function handler(req, res) {
-  // Import and run the main server
-  const { default: app } = await import('../server.js');
-  return app(req, res);
-}
