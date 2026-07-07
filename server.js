5cd4fa409a8ea7851fc9e2a250cbf0de89d6637b
Ssenkula Rivan <ssenkula.rivan@gmail.com>
2026-07-06 18:39:38 -0700

    Revert port for Render deployment

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 5000;
 
 // Orchestration State
 let agentState = {
